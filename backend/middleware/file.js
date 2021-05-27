9e8db8e315c307e6e689ab5a7b8827ecb6da8867
matheeshaYapa <ylmatheesha@gmail.com>
2021-05-27 19:01:40 +0530

    final touches

--- backend/middleware/file.js
+++ backend/middleware/file.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
     if (isValid) {
       error = null;
     }
-    callback(error, 'backend/images');
+    callback(error, 'images');
   },
   filename: (req, file, callback) => {
     const name = file.originalname.toLowerCase().split(' ').join('-');
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://postappmeanbackend-env.eba-ffuujpyu.us-east-2.elasticbeanstalk.com/api'
 };
